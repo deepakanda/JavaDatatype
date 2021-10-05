27ebffdec37d7ff3f892e6ff3b890c8aec31dfa9
deepakanda <deepadeeps88@gmail.com>
2021-10-05 20:39:32 +0530

    Git Project Success

---
+++ src/org/datatypes/Data_Types.java
@@ -0,0 +1,28 @@
+package org.datatypes;
+
+public class Data_Types {
+	public static void main(String[] args) {
+		byte b=33;
+		short s=2233;
+		int a=98848540;
+		long l= 153479478423l;
+		char ch='a';
+		float x=3;
+		double d=2.236;
+	    boolean boo=true;
+	    String str=("I Love My Family");
+	    System.out.println(l);
+	    System.out.println(b);
+	    System.out.println(a);
+	    System.out.println(s);
+	    System.out.println(ch);
+		System.out.println(boo);
+		System.out.println(d);
+		System.out.println(x);
+		System.out.println(str);
+				
+				
+			
+	}
+
+}
